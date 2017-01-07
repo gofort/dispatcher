5a1c39fd4953a55cb3ff605044e651fc9cc4c529
Bogdan Kurnosov <kurnosov_bogdan@hotmail.com>
2017-01-07 11:37:50 +0300

    Fixed permissions for test script

--- go.test.sh
+++ go.test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
+for d in $(go list ./... | grep -v -e vendor -e examples); do
     go test -v -race -coverprofile=profile.out -covermode=count $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
